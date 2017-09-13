f3dc69681f03ceb2443096da18d6a2da35291cd3
Tong <zwstcoy@gmail.com>
2017-09-12 21:06:50 -0400

    Add files via upload

--- java/com/imagination/tong/mrjumper/GameView.java
+++ java/com/imagination/tong/mrjumper/GameView.java
@@ -2,6 +2,7 @@ package com.imagination.tong.mrjumper;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -20,7 +21,19 @@ public class GameView extends SurfaceView implements Runnable{
     public GameView(Context context) {
         super(context);
         holder=getHolder();
-        
+        paint=new Paint();
+        player=new Player(context);
+        player.update(5,5);
+        if(holder.getSurface().isValid()){
+            canvas=holder.lockCanvas();
+
+            canvas.drawColor(Color.argb(255,0,0,0));
+
+            canvas.drawBitmap(player.getBitMap(),player.getX(),player.getY(),paint);
+
+            holder.unlockCanvasAndPost(canvas);
+
+        }
 
     }
 
